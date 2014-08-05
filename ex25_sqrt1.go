476862d22e4bff5045826b98086fe23824098f73
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-08-05 18:49:18 +0000

    Add exercise #25 (sqrt in 10 iterations)

---
+++ ex25_sqrt1.go
@@ -0,0 +1,23 @@
+
+
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func Sqrt(x float64) float64 {
+  z0 := 6 * math.Pow(10, 2)
+  z := z0
+
+  for i := 0; i < 10; i++ {
+    z = z - ( (z * z - x) / (2 * z) )
+  }
+
+  return z
+}
+
+func main() {
+  fmt.Println(Sqrt(2))
+}
