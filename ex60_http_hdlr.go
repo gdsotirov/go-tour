1b5fd3ae2054684bb11a416312a13efdfeb53471
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-08-06 12:58:29 +0000

    Add exercise #60 (http handler)

---
+++ ex60_http_hdlr.go
@@ -0,0 +1,34 @@
+/* Exercise: HTTP Handlers
+ * See http://tour.golang.org/#60
+ * Description: Web server handling specific paths
+ */
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+type String string
+
+type Struct struct {
+    Greeting string
+    Punct    string
+    Who      string
+}
+
+func (str String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, str)
+}
+
+func (sct Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, sct.Greeting, sct.Punct, sct.Who)
+}
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+    http.ListenAndServe("localhost:4000", nil)
+}
+
