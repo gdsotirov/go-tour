d0d14a7dba659ad117b83e8503927b28d39f3b47
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-08-06 12:57:44 +0000

    Add exercise #59 (http)

---
+++ ex59_http.go
@@ -0,0 +1,23 @@
+/* Exercise: Web servers
+ * See http://tour.golang.org/#59
+ * Description: Simple web server that prints a greeting
+ */
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello!")
+}
+
+func main() {
+    var h Hello
+    http.ListenAndServe("localhost:4000", h)
+}
+
