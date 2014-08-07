069f4c0a1575e9b45cded4569ae31dcb34c01a55
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-08-07 13:17:11 +0000

    Add exercise #62 (image)

---
+++ ex62_pic2.go
@@ -0,0 +1,40 @@
+/* Exercise: Images
+ * See http://tour.golang.org/#62
+ * Description: Generate and display image with implementation of image.Image
+ */
+
+package main
+
+import (
+    "code.google.com/p/go-tour/pic"
+    "image"
+    "image/color"
+)
+
+type Image struct {
+    width  int
+    height int
+}
+
+func (i Image) ColorModel() color.Model {
+    return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, i.width, i.height)
+}
+
+func (i Image) At(x, y int) color.Color {
+    red  := uint8(x)
+    green:= uint8(y)
+    /* You could experiment with values of blue between 0 and 255... */
+    var blue uint8 = 255
+    /* and value sof alpha in the same range */
+    var alpha uint8 = 255
+    return color.RGBA{red, green, blue, alpha}
+}
+
+func main() {
+    m := Image{255, 255}
+    pic.ShowImage(m)
+}
