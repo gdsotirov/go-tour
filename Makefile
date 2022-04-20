02a34da4338427e410f4b466c75681c73ce6418c
Georgi D. Sotirov <gdsotirov@gmail.com>
2022-04-20 18:26:45 +0300

    Implement reader exercise, add to Makefile

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all: build
 
 BINS=ex_fc_8_sqrt1 ex_fc_8_sqrt2 \
      ex_mtp_18_pic ex_mtp_23_wc ex_mtp_26_fib \
-     ex_mtd_18_ipaddr ex_mtd_20_sqrt_err ex_mtd_23_rot13 ex_mtd_25_pic2 \
+     ex_mtd_18_ipaddr ex_mtd_20_sqrt_err ex_mtd_22_reader ex_mtd_23_rot13 ex_mtd_25_pic2 \
      ex_cnc_8_tree ex_cnc_10_crawler
 
 build: $(BINS)
@@ -29,6 +29,9 @@ ex_mtd_18_ipaddr: ex_mtd_18_ipaddr.go
 	$(GOBUILD) $<
 
 ex_mtd_20_sqrt_err: ex_mtd_20_sqrt_err.go
+	$(GOBUILD) $<
+
+ex_mtd_22_reader: ex_mtd_22_reader.go
 	$(GOBUILD) $<
 
 ex_mtd_23_rot13: ex_mtd_23_rot13.go
--- ex_mtd_22_reader.go
+++ ex_mtd_22_reader.go
@@ -1,12 +1,26 @@
-/* From  https://go.dev/tour/methods/22 */
+/* Exercise: Readers
+ * See https://go.dev/tour/methods/22
+ * Description: Implement a Reader type that emits an infinite stream of the ASCII character 'A'.
+ */
+
 package main
 
 import "golang.org/x/tour/reader"
 
-type MyReader struct{}
+type MyReader struct {
+    c   byte    // an ASCII character
+}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (r *MyReader) Read(b []byte) (n int, err error) {
+    for i := range b {
+        b[i] = r.c
+    }
+
+    return len(b), nil // never return io.EOF
+}
 
 func main() {
-	reader.Validate(MyReader{})
+    reader.Validate(&MyReader{'A'})
+    reader.Validate(&MyReader{'B'}) // got byte 42 at offset 0, want 'A'
 }
+
