fe6ca79d0dbf1aad5acef9876ae4d5019ad539f9
Georgi D. Sotirov <gdsotirov@gmail.com>
2022-04-21 17:18:21 +0300

    Add original text of generic types exercise

---
+++ ex_gen_2_list.go
@@ -0,0 +1,13 @@
+/* From https://go.dev/tour/generics/2 */
+
+package main
+
+// List represents a singly-linked list that holds
+// values of any type.
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
+
+func main() {
+}
